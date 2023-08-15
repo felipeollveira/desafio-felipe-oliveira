47b56d46a1d75aafcff763dde35eeb3b86562014
f <110308764+felipeollveira@users.noreply.github.com>
2023-08-15 17:15:45 -0300

    update error

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -3,44 +3,43 @@ import { cardapio } from "./cardapio.js";
 class CaixaDaLanchonete {
     calcularValorDaCompra(metodoDePagamento, itens) {
         try {
-            //primeira verificacao; se existe algum item no carrinho
+            // Primeira verificação: se existe algum item no carrinho
             if (itens === undefined || itens.length === 0) {
                 throw new Error('Não há itens no carrinho de compra!');
             }
 
             let valorTotal = 0;
-            let seExtra = true; //só há itens extras até que seja encontrado um
+            let seExtra = true; // Só há itens extras até que seja encontrado um
             
-                 //caça dos itens do carrinho no array cardapio
+            // Caça dos itens do carrinho no array cardapio
             itens.forEach(itemInfo => {
                 const [itemNome, novaQuantidade] = itemInfo.split(',');
-                const itemEscolhido = cardapio.cardapio.find(cardapio => cardapio[itemNome] !== undefined);
-                //se algum item do carrinho nao existir no cardapio
-                 //se 80% da digitação do item estiver correta, a função caça o item digitado pelo item mais parecido no array - em manuntencao
-            if (itemEscolhido === undefined) {
-                //throw new error guarda a msg e é so exibida na chamada do catch
-                //alternativa do console.log visto que ele da um break no sistema
-                throw new Error(`Item inválido!`);
+                const itemEscolhido = cardapio.cardapio.find(cardapioItem => cardapioItem[itemNome] !== undefined);
+                // Se algum item do carrinho não existir no cardapio
+                // Se 80% da digitação do item estiver correta, a função caça o item digitado pelo item mais parecido no array - em manuntencao
+                if (itemEscolhido === undefined) {
+                    // Throw new error guarda a mensagem e é apenas exibida na chamada do catch
+                    // Alternativa ao console.log, visto que ele interrompe o fluxo do sistema
+                    throw new Error(`Item inválido!`);
                 }
-                //Se a quantidade for igual a 0, ou
-                //se o usuario nao digiitar a quantidade, ou
-                //e se o usuario digitar a quantidade em extenso (Ex:três(3))
-                if (novaQuantidade == 0 || novaQuantidade === undefined || isNaN(novaQuantidade)|| !Number.isInteger(Number(novaQuantidade))) {
+                // Se a quantidade for igual a 0, ou
+                // Se o usuário não digitar a quantidade, ou
+                // Se o usuário digitar a quantidade em extenso (Ex: "três(3)")
+                if (novaQuantidade == 0 || novaQuantidade === undefined || isNaN(novaQuantidade) || !Number.isInteger(Number(novaQuantidade))) {
                     throw new Error(`Quantidade inválida!`);
                 }
-                //se um dos itens for do tipo extra, verifica se algum dos itens escolhidos sao seus respectivos principais
+                // Se um dos itens for do tipo extra, verifica se algum dos itens escolhidos são seus respectivos principais
                 if (itemEscolhido.tipo === 'extra') {
                     const nomeDoItemPrincipal = itemEscolhido.principal;
-                //se algum dos itens extras nao tiverem seus respectivos itens principais, é retornado uma msg
-                    if (!itens.some(cardapio => cardapio.split(',')[0] === nomeDoItemPrincipal)) {
-                        console.log(`Item extra não pode ser pedido sem o principal`);
+                    // Se algum dos itens extras não tiverem seus respectivos itens principais, é retornada uma mensagem
+                    if (!itens.some(cardapioItem => cardapioItem.split(',')[0] === nomeDoItemPrincipal)) {
                         seExtra = false;
-                //calculo feito caso itens extras com seus principais
+                    // Cálculo feito caso itens extras com seus principais
                     } else {
                         const precoDoItem = itemEscolhido.preco * novaQuantidade;
                         valorTotal += precoDoItem;
                     }
-                //caso nenhuma das verificações acima sejam verdadeiras, os itens vem direto para essa função
+                // Caso nenhuma das verificações acima seja verdadeira, os itens vão direto para essa função
                 } else {
                     const precoDoItem = itemEscolhido.preco * novaQuantidade;
                     valorTotal += precoDoItem;
@@ -48,9 +47,9 @@ class CaixaDaLanchonete {
             });
 
             if (!seExtra) {
-                return; // Encerra a função se nao houver um item principal
+                return 'Item extra não pode ser pedido sem o principal';
             }
-            //metodos de pagamentos e acrescimo de credito e desconto de dinheiro. debito segue preco normal
+            // Métodos de pagamentos e acréscimo de crédito, e desconto de dinheiro. Débito segue preço normal
             if (metodoDePagamento === 'credito') {
                 valorTotal *= 1.03;
             } else if (metodoDePagamento === 'dinheiro') {
@@ -59,18 +58,19 @@ class CaixaDaLanchonete {
                 throw new Error('Forma de pagamento inválida!');
             }
 
-            //formatacao do dinheiro em reais
+            // Formatação do dinheiro em reais
             const valorFormatado = valorTotal.toFixed(2).replace('.', ',');
-            console.log(`R$ ${valorFormatado}`)
+            return `R$ ${valorFormatado}`;
 
-        //chamada das mensagens de erro
+        // Chamada das mensagens de erro
         } catch (error) {
-            console.error(error.message);
+            return error.message;
         }
     }
 }
 
-const caixa = new CaixaDaLanchonete()
-.calcularValorDaCompra('credito', ['cafe,2','chantily,1']);
+const caixa = new CaixaDaLanchonete();
+const resultado = caixa.calcularValorDaCompra('credito', ['cafe,2','chantily,1']);
+console.log(resultado);
 
 export { CaixaDaLanchonete };
